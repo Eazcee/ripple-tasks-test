533645d6cb9934ce4e0a47b09976c7f0522aeb03
Eazcee <143572290+Eazcee@users.noreply.github.com>
2025-08-09 15:21:57 -0500

    -removed duplicate header from rendering in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LayoutComponent } from './components/layout/layout.component';
+//no need to import the layout component because we are using the router-outlet to export the header
+//import { LayoutComponent } from './components/layout/layout.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, LayoutComponent],
-  template: '<app-layout></app-layout>'
+ selector: 'app-root',
+ standalone: true,
+ //only use the router-outlet to export the header
+ imports: [RouterOutlet], //LayoutComponent],
+ //changed to router-outlet to export the header
+ template: '<router-outlet></router-outlet>'
 })
 export class AppComponent {
-  title = 'Task Management System';
+ title = 'Task Management System';
 }
